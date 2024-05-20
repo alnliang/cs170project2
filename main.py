b9d5b01d2d0b29dd32f9600836b7b18b80407cd4
CharlesEOhagin <charles.ohagin@gmail.com>
2024-05-19 17:45:48 -0700

    ui

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from node import Node 
 import random
+print("Welcome to Charles and Alan's Feature Selection Algorithm")
+option = int(input("Please enter total number of features"))
+print("Type the number on the algorithm you want to run: 1 for foward, 2 for backward, 3 for speical")
+print("use no features and \"random\" evaluation, I get an accuracy of ")
+print("Beginning Search")

253e96dc1b22557a00d0c92c8cdb5c3f5bdb3f7c
alan <alqliang@gmail.com>
2024-05-19 21:07:16 -0700

    backwards

--- main.py
+++ main.py
@@ -11,7 +11,32 @@ def forward(feature):
         continue
 
     return trace
-def backward():
+def backward(num_features):
+    trace = []
+    explored = []
+    features = [i for i in range(1, num_features + 1)]
+    curr_node = Node(features)
+    max = curr_node.get_accuracy()
+    maxPrev = curr_node.get_accuracy()
+    trace.append(curr_node)
+    print(f"Using all features and random evaluation, I get an accuracy of {max}%\n")
+    while len(curr_node.subset) != 0:
+        prev = curr_node.get_prev_states(num_features)
+        explored.append(curr_node.subset)
+        for node in prev:
+            if node.subset in explored:
+                continue
+            trace.append(node)
+            accuracy = node.get_accuracy()
+            print(f"Using feature(s) {node.subset} accuracy is {accuracy}%\n")
+            if(accuracy > max):
+                max = accuracy
+                curr_node = node
+        if(max == maxPrev):
+            print("Warning, accuracy has decreased!\n")
+            break
+        maxPrev = max
+        print(f"Feature set {curr_node.subset} was the best with an accuracy of {max}%")     
     return
 def special():
     return
@@ -22,6 +47,7 @@ if algo_option == 1:
     trace = forward()
     print(f"use no features and \"random\" evaluation, I get an accuracy of {trace[0].accuracy} %")
 if algo_option == 2:
-    trace = backward()
-    print(f"use all features and \"random\" evaluation, I get an accuracy of {trace[0].accuracy} %")
+    # trace = backward()
+    # print(f"use all features and \"random\" evaluation, I get an accuracy of {trace[0].accuracy} %")
+    backward(features)
 print("Beginning Search")
--- node.py
+++ node.py
@@ -5,16 +5,22 @@ class Node:
         self.subset = features
         self.accuracy = 0
         self.next = []
+        self.prev = []
     #change get accuracy later, psuedo code
     def get_accuracy(self):
         self.accuracy =round(random.random()*100,3)
+        return self.accuracy
     #to get additional_feature
     def additional_feature(self, num_features):
         all_feature = [i for i in range(1, num_features + 1)]
         subset = self.subset
         additonal_feature = [i for i in all_feature if i not in subset]
         return additonal_feature
-
+    def remove_feature(self, num_features):
+        all_feature = [i for i in range(1, num_features + 1)]
+        subset = self.subset
+        removable_feature = [i for i in all_feature if i in subset]
+        return removable_feature
     #features are the features in the current node, while num_features are the totat number of features aviable
     #for instance a current state can just be 1 but has a total number of features of 4
     def get_next_states(self, num_feature):
@@ -23,16 +29,37 @@ class Node:
             new_subset = copy.deepcopy(self.subset)
             new_subset.append(feature)
             self.next.append(new_subset)
-
+        return self.next
+    def get_prev_states(self, num_feature):
+        removable_feature = self.remove_feature(num_feature)
+        for feature in removable_feature:
+            new_subset = copy.deepcopy(self.subset)
+            new_subset.remove(feature)
+            newNode = Node(new_subset)
+            self.prev.append(newNode)
+        return self.prev
+    def getHighestPrevAccuracy(self):
+        max = 0
+        for node in self.prev:
+            if node.get_accuracy > max:
+                max = node.get_accuracy
+        return max
+    def printAllPrev(self):
+        for prevNode in self.prev:
+            print(prevNode.subset)
+        
 
 n1 = Node([])
-print(n1.additional_feature(4))
+#print(n1.additional_feature(4))
 print(n1.get_next_states(4))
 n2 = Node([1])
-print(n2.additional_feature(4))
+#print(n2.additional_feature(4))
+print(n2.get_next_states(4))
 n3 = Node([1,2])
 n4 = Node([1,2,3])
 n5 = Node([1,2,3,4])
-print(len(n1.subset))
+n5.get_prev_states(4)
+n5.printAllPrev()
+# print(len(n1.subset))
         
 

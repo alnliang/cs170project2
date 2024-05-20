595ffcead67b7386c542c4c7dbfe358b8691f48a
CharlesEOhagin <charles.ohagin@gmail.com>
2024-05-19 19:16:32 -0700

    added node function and tested

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from node import Node 
 import random
+def forward(feature):
+    trace = []
+    explored = []
+    intitial_node = Node({})
+    intitial_node.get_accuracy()
+    trace.append(intitial_node)
+    reached_end_state =False
+    while reached_end_state == False:
+        if len(explored[0] == feature):
+            reached_end_state = False
+            break
+        
+    return trace
+def backward():
+    return
+def special():
+    return
 print("Welcome to Charles and Alan's Feature Selection Algorithm")
-option = int(input("Please enter total number of features"))
-print("Type the number on the algorithm you want to run: 1 for foward, 2 for backward, 3 for speical")
-print("use no features and \"random\" evaluation, I get an accuracy of ")
+features = int(input("Please enter total number of features"))
+algo_option = int(input("Type the number on the algorithm you want to run: 1 for foward, 2 for backward, 3 for special"))
+if algo_option == 1:
+    trace = forward()
+    print(f"use no features and \"random\" evaluation, I get an accuracy of {trace[0].accuracy} %")
+if algo_option == 2:
+    trace = backward()
+    print(f"use all features and \"random\" evaluation, I get an accuracy of {trace[0].accuracy} %")
 print("Beginning Search")
--- node.py
+++ node.py
@@ -1,8 +1,41 @@
 import random
+import copy
 class Node:
-    def __init__(self,subset):
-        self.subset = subset
+    def __init__(self,features):
+        self.subset = features
         self.accuracy = 0
+        self.next = []
     #change get accuracy later, psuedo code
     def get_accuracy(self):
         self.accuracy =round(random.random()*100,3)
+    #to get additional_feature
+    def additional_feature(self, num_features):
+        all_feature = [i for i in range(1, num_features + 1)]
+        subset = self.subset
+        additonal_feature = [i for i in all_feature if i not in subset]
+        return additonal_feature
+
+    #features are the features in the current node, while num_features are the totat number of features aviable
+    #for instance a current state can just be 1 but has a total number of features of 4
+    def get_next_states(self, num_feature):
+        additonal_feature = self.additional_feature(num_feature)
+        for feature in additonal_feature:
+            new_subset = copy.deepcopy(self.subset)
+            new_subset.append(feature)
+            self.next.append(new_subset)
+
+
+n1 = Node([])
+print(n1.additional_feature(4))
+print(n1.get_next_states(4))
+n2 = Node([1])
+print(n2.additional_feature(4))
+n3 = Node([1,2])
+n4 = Node([1,2,3])
+n5 = Node([1,2,3,4])
+print(len(n1.subset))
+        
+list1 = [1, 2, 3, 4]
+list2 = [1, 2]
+unique_to_list1 = [i for i in list1 if i not in list2]
+print(unique_to_list1)
